d6f65681505186766102765437c9434d2900dc13
Gunna <rohitreddy.gunna@capgemini.com>
2019-07-11 13:25:03 +0530

    del and update (AC1 and Lead layout)

---
+++ .sfdx/typings/lwc/apex/ContactController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/ContactController.findContacts" {
+  export default function findContacts(param: {searchKey: any}): Promise<any>;
+}
+declare module "@salesforce/apex/ContactController.getSingleContact" {
+  export default function getSingleContact(): Promise<any>;
+}
